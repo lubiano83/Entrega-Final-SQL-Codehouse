bad374d9bbc1cfda9fc86072965e4941fc0dd496
lubiano83 <josepablolubiano@lubiano83.local>
2025-01-21 14:46:01 -0300

    ajustes varios

--- Primera-Entrega.sql
+++ Primera-Entrega.sql
@@ -4,48 +4,48 @@ USE AutoPartsStore;
 
 -- Tabla Categorías
 CREATE TABLE Categories (
-    CategoryID INT AUTO_INCREMENT PRIMARY KEY, -- Clave primaria
-    CategoryName VARCHAR(50) NOT NULL,        -- Nombre de la categoría
-    Description VARCHAR(100)                          -- Descripción de la categoría
+    CategoryID INT AUTO_INCREMENT PRIMARY KEY,
+    CategoryName VARCHAR(50) NOT NULL,
+    Description VARCHAR(100)
 );
 
 -- Tabla Productos
 CREATE TABLE Products (
-    ProductID INT AUTO_INCREMENT PRIMARY KEY, -- Clave primaria
-    ProductName VARCHAR(50) NOT NULL,        -- Nombre del producto
-    Description VARCHAR(100),                         -- Descripción del producto
-    Price DECIMAL(10, 2) NOT NULL,            -- Precio del producto
-    Stock INT NOT NULL,                       -- Cantidad disponible
-    CategoryID INT,                           -- Relación con Categorías
+    ProductID INT AUTO_INCREMENT PRIMARY KEY,
+    ProductName VARCHAR(50) NOT NULL,
+    Description VARCHAR(100),
+    Price DECIMAL(10, 2) NOT NULL,
+    Stock INT NOT NULL,
+    CategoryID INT,
     FOREIGN KEY (CategoryID) REFERENCES Categories(CategoryID)
 );
 
 -- Tabla Clientes
 CREATE TABLE Customers (
-    CustomerID INT AUTO_INCREMENT PRIMARY KEY, -- Clave primaria
-    FirstName VARCHAR(50) NOT NULL,           -- Nombre del cliente
-    LastName VARCHAR(50) NOT NULL,            -- Apellido del cliente
-    Email VARCHAR(50) UNIQUE NOT NULL,        -- Correo electrónico
-    Phone VARCHAR(12),                         -- Teléfono
-    Address VARCHAR(75)                               -- Dirección
+    CustomerID INT AUTO_INCREMENT PRIMARY KEY,
+    FirstName VARCHAR(50) NOT NULL,
+    LastName VARCHAR(50) NOT NULL,
+    Email VARCHAR(50) UNIQUE NOT NULL,
+    Phone VARCHAR(12),
+    Address VARCHAR(75)
 );
 
 -- Tabla Órdenes
 CREATE TABLE Orders (
-    OrderID INT AUTO_INCREMENT PRIMARY KEY, -- Clave primaria
-    OrderDate DATETIME NOT NULL,            -- Fecha y hora de la orden
-    CustomerID INT,                         -- Relación con Clientes
-    TotalAmount DECIMAL(10, 2) NOT NULL,    -- Monto total de la orden
+    OrderID INT AUTO_INCREMENT PRIMARY KEY,
+    OrderDate DATETIME NOT NULL,
+    CustomerID INT,
+    TotalAmount DECIMAL(10, 2) NOT NULL,
     FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID)
 );
 
 -- Tabla Detalles de Órdenes
 CREATE TABLE OrderDetails (
-    OrderDetailID INT AUTO_INCREMENT PRIMARY KEY, -- Clave primaria
-    OrderID INT,                                  -- Relación con Órdenes
-    ProductID INT,                                -- Relación con Productos
-    Quantity INT NOT NULL,                        -- Cantidad de productos
-    UnitPrice DECIMAL(10, 2) NOT NULL,            -- Precio unitario
+    OrderDetailID INT AUTO_INCREMENT PRIMARY KEY,
+    OrderID INT,
+    ProductID INT,
+    Quantity INT NOT NULL,
+    UnitPrice DECIMAL(10, 2) NOT NULL,
     FOREIGN KEY (OrderID) REFERENCES Orders(OrderID),
     FOREIGN KEY (ProductID) REFERENCES Products(ProductID)
 );
